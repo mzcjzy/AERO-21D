ea58613087c51e210bb26567c794b6423d518f38
wujq77 <1297338838@qq.com>
2021-04-30 19:50:20 +0800

    惯导部分代码提交

---
+++ catkin_diff_gps/src/diff_gps/src/imupublisher.cpp
@@ -0,0 +1,273 @@
+/*   programmed by Liqiangwei
+     modified on 2018.8.30
+This node is used to publish the date collected from the xwyd GI5651 sensor to ROS master.
+usage:1.identify the port you used
+      2.change the portname 
+      3.change the Baudrate
+      4.the looprate is set in line 71   */    
+
+#include <ros/ros.h>
+#include "linuxserial.h"
+#include <iostream>
+#include <queue>
+#include <sensor_msgs/Imu.h>
+#include <tf/tf.h>
+#include <geometry_msgs/Vector3Stamped.h>
+#include <sensor_msgs/NavSatFix.h>
+
+using namespace std;
+
+//宏定义
+#define FALSE  -1
+#define TRUE   0
+
+struct GTIMU{
+    unsigned short int GPSweek;
+    unsigned int GPStime;
+    double GyroX;
+    double GyroY;
+    double Gyroz;
+    double AccX;
+    double AccY;
+    double AccZ;
+    float Yaw;
+    float Pitch;
+    float Roll;
+    unsigned int latitude;
+    unsigned int longitude;
+    unsigned int altitude;
+    float Ve;
+    float Va;
+    float Vu;
+};
+
+
+queue<unsigned char> tembuff_1;
+queue<unsigned char> tembuff_2;
+
+void bufproc(const unsigned char *revbuff, int lenth, queue<unsigned char> tembuff, int datatype);
+void IMU_datatran(sensor_msgs::Imu &temimu, GTIMU const temimudata, ros::Time Time);
+void GPS_datatran(sensor_msgs::NavSatFix &tmpGps, GTIMU const temimudata, ros::Time const Time);
+void Vel_datatran(geometry_msgs::Vector3Stamped tmpVel, GTIMU const temimudata, ros::Time const Time);
+
+
+GTIMU IMUinfo;
+sensor_msgs::Imu imu;
+sensor_msgs::NavSatFix gps;
+geometry_msgs::Vector3Stamped velocity;
+
+int main(int argc,char *argv[]){
+    
+    //variables for port1
+    int fd1;                            //文件描述符
+    int err1;                           //返回调用函数的状态
+    int len1;
+    unsigned char rcv_buf_1[2000] = {};
+
+    //variables for port2
+    int fd2;                            //文件描述符
+    int err2;                           //返回调用函数的状态
+    int len2;
+    unsigned char rcv_buf_2[2000] = {};
+
+    int counter=0;
+    
+    //init ros
+    ros::init(argc, argv, "IMUstatepublisher");
+    ros::NodeHandle handle;
+    ros::Publisher IMU_pub = handle.advertise<sensor_msgs::Imu>("IMU_data", 1000);
+    ros::Publisher GPS_pub = handle.advertise<sensor_msgs::NavSatFix>("GPS_data", 1000);
+    ros::Publisher Velocity_pub = handle.advertise<geometry_msgs::Vector3Stamped>("Velocity_data", 1000);
+
+
+    roserialhandler roserial1("/dev/ttyUSB0",9600);
+    fd1 = roserial1.UART0_Open();
+    // roserialhandler roserial2("/dev/ttyUSB1",115200);
+    // fd2 = roserial2.UART0_Open();
+
+    do{
+        err1 = roserial1.UART0_Init(0,8,1,'N');
+        //err2 = roserial2.UART0_Init(0,8,1,'N');
+        printf("Set Port Exactly!\n");
+        sleep(2);
+    }while(FALSE == err1 || FALSE == fd1/* || FALSE == err2 || FALSE == fd2*/);
+
+    printf("IMU_publisher starts successfully");
+
+    //loop body
+    while (ros::ok()){
+        len1 = roserial1.UART0_Recv( rcv_buf_1,2000);
+        if(len1 > 0){bufproc(rcv_buf_1,len1,tembuff_1,1);}
+        else{printf("/dev/ttyUSB0 cannot receive data\n");}
+
+        // len2 = roserial2.UART0_Recv(rcv_buf_2,2000);
+        // if(len2 > 0){bufproc(rcv_buf_2,len2,tembuff_2,2);}
+        // else{printf("/dev/ttyUSB1 cannot receive data\n");}
+
+        ros::Time nowTime = ros::Time::now();
+        IMU_datatran(imu, IMUinfo, nowTime);
+        GPS_datatran(gps, IMUinfo, nowTime);
+        Vel_datatran(velocity, IMUinfo, nowTime);
+
+        // TODO:值的筛选
+
+
+        IMU_pub.publish(imu);
+        GPS_pub.publish(gps);
+        Velocity_pub.publish(velocity);
+        
+        cout<<"counter is "<<counter<<endl;
+        counter++;
+
+        usleep(1000);
+    }
+}
+
+
+void bufproc(const unsigned char *revbuff,int lenth,queue<unsigned char> tembuff,int datatype){
+    int startflag = 0;
+    int startindex;
+    int pushflag =0;
+    int lenthoftembuff = tembuff.size();
+    unsigned char procbuff[2000] = {};
+
+    if(lenthoftembuff>0){
+        for(int i=0;i < lenthoftembuff;i++){
+            procbuff[i] = tembuff.front();
+            tembuff.pop();
+        }
+        for(int j = 0;j <lenth;j++){
+            procbuff[lenthoftembuff+1+j] = revbuff[j];
+        }
+    }
+    if(lenthoftembuff == 0){
+        for(int j = 0;j <lenth;j++){
+            procbuff[j] = revbuff[j];
+        }
+    }
+    for(int i = 0;i<(lenthoftembuff + lenth);i++)
+    {
+        if (procbuff[i] == 170 ){
+            if(i<(lenthoftembuff + lenth-1)){
+                if(procbuff[i+1] == 85)
+                    startflag = 1;
+                    startindex = i;
+            }
+            if(i == (lenthoftembuff + lenth-1) ){
+                tembuff.push(procbuff[i]);
+                pushflag =1;
+            }
+        }
+        if ((lenth+lenthoftembuff - startindex <= 59) && (startflag == 1)&&(pushflag == 0)){
+            for(int j = startindex;j<=lenth;j++){
+                tembuff.push(procbuff[j]);
+            }
+            pushflag =1;
+        }
+        if (startflag == 1){
+            //GTIMU_BIN
+            if (datatype == 1){
+                IMUinfo.GPSweek= *(unsigned short int*) (&(procbuff[startindex+3+0]));
+                IMUinfo.GPStime= *(unsigned int*) (&(procbuff[startindex+3+2]));
+                IMUinfo.GyroX = *(double *) (&(procbuff[startindex+3+6]));
+                IMUinfo.GyroY = *(double *) (&(procbuff[startindex+3+14]));
+                IMUinfo.Gyroz = *(double *) (&(procbuff[startindex+3+22]));
+                IMUinfo.AccX = *(double *) (&(procbuff[startindex+3+30]));
+                IMUinfo.AccY = *(double *) (&(procbuff[startindex+3+38]));
+                IMUinfo.AccZ = *(double *) (&(procbuff[startindex+3+46]));
+            }
+            //GPFPD_GIN
+            if(datatype == 2){
+                IMUinfo.Yaw = *(float *)(&(procbuff[startindex+3+6]));
+                IMUinfo.Pitch = *(float *)(&(procbuff[startindex+3+10]));
+                IMUinfo.Roll = *(float *)(&(procbuff[startindex+3+14]));
+                IMUinfo.latitude = *(unsigned int *)(&(procbuff[startindex+3+18]));
+                IMUinfo.longitude = *(unsigned int *)(&(procbuff[startindex+3+22]));
+                IMUinfo.altitude = *(unsigned int *)(&(procbuff[startindex+3+26]));
+                IMUinfo.Ve = *(float *)(&(procbuff[startindex+3+30]));
+                IMUinfo.Va = *(float *)(&(procbuff[startindex+3+34]));
+                IMUinfo.Vu = *(float *)(&(procbuff[startindex+3+38]));
+            }
+            startflag = 0;
+            startindex = 0;
+        }
+    }
+    procbuff[2000]={'+'};
+    startflag = 0;
+    startindex = 0;
+    lenthoftembuff = 0;
+    return;
+}
+
+void IMU_datatran(sensor_msgs::Imu &temimu, GTIMU const temimudata, ros::Time const Time){
+    tf::Quaternion q = tf::createQuaternionFromRPY((temimudata.Roll*3.1415926/180),(temimudata.Pitch*3.1415926/180), (temimudata.Yaw*3.1415926/180));
+    
+    temimu.header.stamp = Time;
+    temimu.header.frame_id = "base_IMU";
+    temimu.orientation.x = q.x();
+    temimu.orientation.y = q.y();
+    temimu.orientation.z = q.z();
+    temimu.orientation.w = q.w();
+    temimu.linear_acceleration.x = temimudata.AccX;
+    temimu.linear_acceleration.y = temimudata.AccY;
+    temimu.linear_acceleration.z = temimudata.AccZ;
+    temimu.angular_velocity.x = temimudata.GyroX;
+    temimu.angular_velocity.y = temimudata.GyroY;
+    temimu.angular_velocity.z = temimudata.Gyroz;
+    /*
+    ROS_INFO("Send the IMU: [%f, %f, %f, \n %f, %f, %f, \n %f, %f, %f, %f]",
+             temimu.orientation.x,
+             temimu.orientation.y,
+             temimu.orientation.z,
+             temimu.orientation.w,
+             temimu.linear_acceleration.x,
+             temimu.linear_acceleration.y,
+             temimu.linear_acceleration.z,
+             temimu.angular_velocity.x,
+             temimu.angular_velocity.y,
+             temimu.angular_velocity.z);
+    */
+    return;
+
+}
+
+void GPS_datatran(sensor_msgs::NavSatFix &tmpGps, GTIMU const temimudata, ros::Time const Time){
+    if (temimudata.longitude == 0 ||
+            temimudata.latitude == 0 ||
+            temimudata.altitude == 0 ||
+            temimudata.longitude > 180*10000000.0 ||
+            temimudata.latitude > 90*10000000.0 ||
+            temimudata.altitude > 100*1000.0
+            )
+    {return;}
+
+    tmpGps.header.stamp = Time;
+    tmpGps.longitude = temimudata.longitude;
+    tmpGps.latitude = temimudata.latitude;
+    tmpGps.altitude = temimudata.altitude;
+
+    ROS_INFO("Send the GPS location: [%f, %f, %f]",
+                    tmpGps.longitude/10000000.0, tmpGps.latitude/10000000.0, tmpGps.altitude/1000.0);
+
+    return;
+
+}
+
+void Vel_datatran(geometry_msgs::Vector3Stamped tmpVel, GTIMU const temimudata, ros::Time const Time){
+    tmpVel.header.stamp = Time;
+
+    if (abs(temimudata.Ve) <= 0.000000001
+        && abs(temimudata.Ve) <= 0.000000001
+        && abs(temimudata.Ve) <= 0.000000001)
+    {
+        return;
+    }
+    tmpVel.vector.x = temimudata.Ve;   //东向
+    tmpVel.vector.y = temimudata.Va;   //北向
+    tmpVel.vector.z = temimudata.Vu;   //天向
+
+    ROS_INFO("Send the velocity: [%f, %f, %f]", tmpVel.vector.x, tmpVel.vector.y, tmpVel.vector.z);
+
+    return;
+
+}
---
+++ catkin_diff_gps/src/diff_gps/src/tfbroadcaster.cpp
@@ -0,0 +1,299 @@
+//
+// Created by lhospital on 18-10-28.
+// Contributer Liqiangwei
+/* 2018年10月28日修改,增加程序初始化流程，首先程序判读是否能够读取IMU和GPS的数据，若能够读取到数据，则进入坐标原点
+   初始化流程，程序会判断来自GPS节点的读数，当读数保持不变4秒以上，将数值作为坐标原点，程序进入循环体，向ROS发送MAP与
+   BASE_LINk之间的TF关系。若一直没有数据传入，则程序在9秒后自动退出，并显示"NO data from GPS/IMU"
+*/
+//
+//程序功能：接收GPS和IMU数据,创建并发布tf信息
+
+//***************************************************************************************************
+//***************************************************************************************************
+
+#include <ros/ros.h>
+#include <geometry_msgs/PointStamped.h>
+#include <std_msgs/Bool.h>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/LinearMath/Quaternion.h>
+#include <tf/LinearMath/Vector3.h>
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/NavSatFix.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <iostream>
+
+using namespace message_filters;
+using namespace sensor_msgs;
+using namespace std;
+
+const double PI = 3.1415926;
+
+class tf_global {
+    private:
+        ros::NodeHandle n_;
+         
+        //话题订阅
+        ros::Subscriber imu_sub;
+        ros::Subscriber gps_sub;
+        //话题发送
+	    ros::Publisher tf_ready = n_.advertise<std_msgs::Bool>("tf_ready", 1000);
+        int readyOk = 0;//存储tf_ready话题是否发布   
+        tf::TransformBroadcaster tf_pub_;//创建tf广播器
+
+        //起始位置经纬度
+        //TODO:写一个初始点经纬度读取函数
+        double long_origin = 0;
+        double la_origin = 0;
+        //目标点经纬度
+        double long_target = 0;
+        double la_target = 0;
+        //旋转矩阵
+        geometry_msgs::Quaternion rotation;//接收imu四元数数据
+        //判断gps、imu publishing节点是否有数据发出
+        bool imuready = false;
+        bool gpsready = false;
+        bool imugpsready();
+        //读取初始经纬度
+        void setOrigin(const double longitude ,const double latitude);
+        //经纬度转高斯坐标
+        void GPStoXY_update(double Latitude, double Longitude, double *Gauss_X, double *Gauss_Y);
+        //计算相对坐标
+        void toRelative(double x_origin, double y_origin, double x_target, double y_target, double &x_relative, double &y_relative);
+
+        //发布tf树
+        void publish_tfTree(const double &x_relative, const double &y_relative);
+
+        //初始化坐标函数
+        bool init_origin();
+
+        //origin初始化成功
+        bool origininitialized = false;
+        //callback函数
+        void callback_imu(const ImuConstPtr &imu);
+        void callback_gps(const NavSatFixConstPtr &gps);
+
+    public:
+        tf_global();
+        ~ tf_global(){};
+        void run();
+};
+
+
+void tf_global::setOrigin(const double longitude ,const double latitude) {
+    this->long_origin = longitude;
+    this->la_origin = latitude;
+}
+
+void tf_global::GPStoXY_update(double Latitude, double Longitude, double *Gauss_X, double *Gauss_Y) {
+    //参数设定
+    double a, b, f, e, e2;
+    a = 6378137.0;
+    b = 6356752.3142;
+    f = 1/298.257223563;
+    e = sqrt(0.0066943799013);
+    e2 = sqrt(0.00673949674227);
+
+    //选定投影带（3度带）
+    int n,L0;
+    double l;
+    n = (Longitude - 1.5)/3 + 1;
+    L0 = n * 3;
+    l = (Longitude - L0)/180.0 * PI;
+
+
+    //高斯投影
+    double A1, A2, A3, A4, A5;
+    A1 = a * (1 - pow(e,2));
+    A2 = A1 * pow(e,2) * 3/2;
+    A3 = A2 * pow(e,2) * 5/4;
+    A4 = A3 * pow(e,2) * 7/6;
+    A5 = A4 * pow(e,2) * 9/8;
+
+    double F1, F2, F3, F4, F5;
+    F1 = 1.0 * A1 + 1.0/2.0 * A2 + 3.0/8.0 * A3 + 5.0/16.0 * A4 + 35.0/128.0 * A5;
+    F2 = 1.0/2.0 * A2 + 1.0/2.0 * A3 + 15.0/32.0 * A4 + 7.0/16.0 * A5;
+    F3 = 1.0/8.0 * A3 + 3.0/16.0 * A4 + 7.0/32.0 * A5;
+    F4 = 1.0/32.0 * A4 + 1.0/16.0 * A5;
+    F5 = 1.0/128.0 * A5;
+
+
+    double T, M, N, P;
+    double B;
+    B=Latitude/180.0*PI;
+    T = tan(B);
+    M = l * cos(B);
+    N = a / sqrt(1 - pow(e,2) * pow(sin(B),2));
+    P = e2 * cos(B);
+
+    double S;
+    S = F1 * B - F2 * sin(2.0 * B) * 1/2.0 + F3 * sin(4.0 * B) * 1/4.0 - F4 * sin(6.0 * B) * 1/6.0 + F5 * sin(8.0 * B) * 1/8.0;
+
+    double x, y;
+    x = S + N * T * (pow(M,2) * 1/2.0 + (5.0 - pow(T,2) + 9.0 * pow(P,2) + 4.0 * pow(P,4)) * pow(M,4.0) * 1/24.0 + (61.0 - 58.0 * pow(T,2) + pow(T,4)) * pow(M,6) * 1/720.0);
+    y = N * (M + (1.0 - pow(T,2) + pow(P,2)) * pow(M,3) * 1/6.0 + (5.0 - 18.0 * pow(T,2) + pow(T,4) + 14.0 * pow(P,2) - 58.0 * pow(T,2) * pow(P,2)) * pow(M,5) * 1/120.0);
+
+    //纵轴修正
+    y = 1000000 * n + 500000 + y;
+
+    //坐标轴转换
+    *Gauss_X = x;
+    *Gauss_Y = y;
+}
+
+/*
+计算当前点与原点的相对坐标，存储于x_relative和y_relative中
+*/
+void tf_global::toRelative(double x_origin, double y_origin, double x_target, double y_target, double &x_relative,
+                           double &y_relative) {
+    x_relative = x_target - x_origin;
+    y_relative = y_target - y_origin;
+}
+
+/*
+发布tf树：车辆坐标系相对于世界坐标系的坐标转换
+*/
+void tf_global::publish_tfTree(const double &x_relative, const double &y_relative){
+    ros::Time time;
+    time = ros::Time::now();//记录当前时间
+
+    //得到参数
+    tf::Vector3 transition(x_relative, y_relative, 0);//记录当前车辆与原点的相对坐标，不计高度
+    tf::Quaternion rotation_tf;//四元数
+    tf::quaternionMsgToTF(this->rotation, rotation_tf);//将IMU的旋转信息存储入rotation_tf中
+
+    //tf_pub_发布消息：车辆相对于原点的旋转、坐标，时间
+    tf_pub_.sendTransform(
+            tf::StampedTransform(
+                    tf::Transform(rotation_tf, transition),
+                    time,
+                    "map",
+                    "base_link"
+                    )
+            );
+}
+
+/*
+接收imu四元数数据，接收成功后，imuready置为true
+*/
+void tf_global::callback_imu(const ImuConstPtr &imu) {
+    this->rotation = imu->orientation;
+    imuready = true;
+    return;
+}
+
+void tf_global::callback_gps(const NavSatFixConstPtr &gps) {
+    //long_target和la_target记录当前车辆经纬度
+    this->long_target = (gps->longitude)/10000000.0;
+    this->la_target = (gps->latitude)/10000000.0;
+
+    //若原点设置成功
+    if(origininitialized){
+        //计算原点与目标点的高斯坐标
+        double x_target_gauss, y_target_gauss;
+        double x_origin_gauss, y_origin_gauss;
+        GPStoXY_update(this->la_origin, this->long_origin, &x_origin_gauss, &y_origin_gauss);
+        GPStoXY_update(this->la_target, this->long_target, &x_target_gauss, &y_target_gauss);
+        //计算相对坐标，存储x,y中
+        double x, y;
+        toRelative(x_target_gauss, y_target_gauss, x_origin_gauss, y_origin_gauss, x, y);
+
+        //发布tf
+        publish_tfTree(x, y);
+    };
+    gpsready = true;
+    return;
+}
+
+void tf_global::run() {
+    //创建发布者tf_ready，发布tf_ready话题，队列长度1000
+    tf_ready = n_.advertise<std_msgs::Bool>("tf_ready", 1000, this);
+    ros::Rate looprate(20); //循环频率20Hz
+
+    bool ready = init_origin(); //设置原点，返回原点是否设置成功  
+
+    //若原点设置成功，在tf_ready话题中发布true，将readyOK置为1
+    //spin写入循环体无意义
+    while(ready==true){
+        if(readyOk == 0) {
+            std_msgs::Bool Ready;
+            Ready.data = true;
+            tf_ready.publish(Ready);
+            readyOk = 1;
+        }
+        //等待IMU_data和GPS_data话题
+        ros::spin();
+        looprate.sleep();
+    };
+    return;
+}
+
+tf_global::tf_global() {
+    //订阅IMU_data和GPS_data，队列长度1
+    imu_sub = n_.subscribe("IMU_data", 1, &tf_global::callback_imu, this);
+    gps_sub = n_.subscribe("GPS_data", 1, &tf_global::callback_gps, this);
+}
+
+/*
+将车辆5秒内不动的点设为原点
+若接收不到imu数据，返回false
+若设置成功，输出提示，返回true
+*/
+bool tf_global::init_origin(){
+    int counter = 0;
+    double long_temp ,la_temp ;//存储临时经纬度数据
+    ros::Rate looprate(1);//循环频率1Hz
+    std::cout<<" Initializing origin position ..."<<std::endl;//输出提示
+
+    //接收订阅的消息并判断是否接收成功
+    do{
+        ros::spinOnce();
+        counter++;
+        if(counter > 2){return false;}//循环三次不成功则返回false
+    }while(imugpsready() == false);
+
+    //判断车辆位置是否移动
+
+    counter = 0;
+
+    do{
+        ros::spinOnce();
+        if(long_temp - long_target > 0.00000000000001 || la_temp - la_target > 0.00000000000001){
+            long_temp = long_target;
+            la_temp = la_target;
+            counter = 0;
+        }
+        if(long_temp - long_target < 0.00000000000001 && la_temp - la_target < 0.00000000000001){
+            counter ++;
+        }
+        looprate.sleep();
+    }while(counter < 5 );
+
+    setOrigin(long_temp,la_temp);//将origin设为当前值（经纬度）
+    origininitialized = true;//初始化成功
+    std::cout<<" Origin position has been initialized successfully \n Program is entering loop "<<std::endl;//输出提示
+    return true;
+}
+
+bool tf_global::imugpsready(){
+    int counter = 0;
+    ros::Rate looprate(1);//循环频率1Hz
+    while((imuready == false) && (gpsready == false)){
+        ros::spinOnce();//接受订阅消息
+        looprate.sleep();
+        counter++;
+        if(counter > 2 && imuready == false && gpsready == false){return false;}//若循环三次不成功，返回false
+    };
+    return true;
+}
+
+int main(int argc, char *argv[]) {
+    ros::init(argc, argv, "tf_map_To_base_link");
+    tf_global tf_global;
+    tf_global.run();
+    std::cout<<"NO data from GPS/IMU"<<std::endl;//不会执行
+    return 1;
+}
