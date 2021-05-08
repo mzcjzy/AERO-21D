a82263f6e2a1692ca7d6ca90a096bc61e05e3824
198265 <1297338838@qq.com>
2021-05-08 09:43:54 +0800

    修改imupublisher.cpp
    处理后数据正确
    问题：数据无法实时更新

--- catkin_diff_gps/src/diff_gps/src/imupublisher.cpp
+++ catkin_diff_gps/src/diff_gps/src/imupublisher.cpp
@@ -73,7 +73,7 @@ int main(int argc,char *argv[]){
     ros::Publisher Velocity_pub = handle.advertise<geometry_msgs::Vector3Stamped>("Velocity_data", 1000);
 
 
-    roserialhandler roserial1("/dev/ttyUSB0",9600);
+    roserialhandler roserial1("/dev/ttyUSB0",115200);
     fd1 = roserial1.UART0_Open();
 
     do{
@@ -108,7 +108,7 @@ int main(int argc,char *argv[]){
         cout<<"counter is "<<counter<<endl;
         counter++;
 
-        usleep(10000);
+        usleep(1000);
     }
 }
 
@@ -116,11 +116,12 @@ int main(int argc,char *argv[]){
 void bufproc(const unsigned char *revbuff,int lenth,queue<unsigned char> tembuff,int datatype){
     //cout<<"enter bufproc"<<endl;
     int startflag = 0;
-    int startindex;
+    int startindex =0;
     int pushflag =0;
     int lenthoftembuff = tembuff.size();
     unsigned char procbuff[2000] = {};
 
+    //如果tembuff中有数据，将数据转移给procbuff，再把revbuff中的数给procbuff
     if(lenthoftembuff>0){
         for(int i=0;i < lenthoftembuff;i++){
             procbuff[i] = tembuff.front();
@@ -130,81 +131,107 @@ void bufproc(const unsigned char *revbuff,int lenth,queue<unsigned char> tembuff
             procbuff[lenthoftembuff+1+j] = revbuff[j];
         }
     }
-    if(lenthoftembuff == 0){
+    //如果tembuff中没有数据，直接将revbuff中的数给procbuff
+    else if(lenthoftembuff == 0){
         for(int j = 0;j <lenth;j++){
             procbuff[j] = revbuff[j];
-            cout<<"procbuff[j]="<<int(revbuff[j])<<endl;
         }
     }
+    //遍历procbuff数组，处理数据
     for(int i = 0;i<(lenthoftembuff + lenth);i++)
     {
        // cout<<"enter loop:find start index"<<endl;
-        if (int(procbuff[i]) == 170){/*aa*/
-            if(i<(lenthoftembuff + lenth-1)){
+
+        if (int(procbuff[i]) == 170)/*aa*/
+        {
+            if(i<(lenthoftembuff + lenth-1))
+            {
                 if(int(procbuff[i+1]) == 85)/*55*/
-                    startflag = 1;
-                    startindex = i;
+                {
+                    if (i<(lenthoftembuff + lenth-2))
+                    {
+                        if (int(procbuff[i+2]) == 5)
+                            {
+                                startflag = 1;
+                                startindex = i;
+                            }
+                    }
+                    else if (i==(lenthoftembuff + lenth-2))
+                    {
+                        tembuff.push(procbuff[i]);
+                        tembuff.push(procbuff[i+1]);
+                        pushflag =1;
+                    }
+                   
+                }
+                    
             }
-            if(i == (lenthoftembuff + lenth-1) ){
+           else  if(i == (lenthoftembuff + lenth-1) ){
                 tembuff.push(procbuff[i]);
                 pushflag =1;
             }
         }
-        if ((lenth+lenthoftembuff - startindex <= 59) && (startflag == 1)&&(pushflag == 0)){
+        if ((lenth+lenthoftembuff - startindex <= 112) && (startflag == 1)&&(pushflag == 0))//数组长度不足且尚未存到tembuff
+        {
             for(int j = startindex;j<=lenth;j++){
                 tembuff.push(procbuff[j]);
             }
-            pushflag =1;
+           
         }
-       // cout<<"startflag="<<startflag<<endl;
-        if (startflag == 1){
-            //GTIMU_BIN
-            if (datatype == 1){
-               cout<<"I am IMU1"<<endl;
-                IMUinfo.GPSweek= *(unsigned short int*) (&(procbuff[startindex+3+0]));
-                IMUinfo.GPStime= *(unsigned int*) (&(procbuff[startindex+3+2]));
-                IMUinfo.GyroX = *(double *) (&(procbuff[startindex+3+6]));
-                IMUinfo.GyroY = *(double *) (&(procbuff[startindex+3+14]));
-               IMUinfo.Gyroz = *(double *) (&(procbuff[startindex+3+22]));
-                IMUinfo.AccX = *(double *) (&(procbuff[startindex+3+30]));
-                IMUinfo.AccY = *(double *) (&(procbuff[startindex+3+38]));
-                IMUinfo.AccZ = *(double *) (&(procbuff[startindex+3+46]));
-                // cout<<"IMUinfo.GPSweek = "<<IMUinfo.GPSweek<<endl;
-                // cout<<"IMUinfo.GPStime = "<<IMUinfo.GPStime<<endl;
-                // cout<<" IMUinfo.GyroX = "<< IMUinfo.GyroX<<endl;
-                // cout<<" IMUinfo.GyroY = "<<IMUinfo.GyroY<<endl;
-                // cout<<"IMUinfo.GyroZ = "<< IMUinfo.Gyroz<<endl;
-                // cout<<"IMUinfo.AccX = "<< IMUinfo.AccX<<endl;
-                // cout<<"  IMUinfo.AccY  = "<<  IMUinfo.AccY <<endl;
-                // cout<<" IMUinfo.AccZ = "<<  IMUinfo.AccZ<<endl;
-            }
-            //GPFPD_GIN
-            // if(datatype == 2){
-            //     cout<<"I am IMU2"<<endl;
-            //     IMUinfo.Yaw = *(float *)(&(procbuff[startindex+3+6]));
-            //     IMUinfo.Pitch = *(float *)(&(procbuff[startindex+3+10]));
-            //     IMUinfo.Roll = *(float *)(&(procbuff[startindex+3+14]));
-            //     IMUinfo.latitude = *(unsigned int *)(&(procbuff[startindex+3+18]));
-            //     IMUinfo.longitude = *(unsigned int *)(&(procbuff[startindex+3+22]));
-            //     IMUinfo.altitude = *(unsigned int *)(&(procbuff[startindex+3+26]));
-            //     IMUinfo.Ve = *(float *)(&(procbuff[startindex+3+30]));
-            //     IMUinfo.Va = *(float *)(&(procbuff[startindex+3+34]));
-            //     IMUinfo.Vu = *(float *)(&(procbuff[startindex+3+38]));
-            // }
+        else if ((lenth+lenthoftembuff - startindex > 112) && (startflag == 1)&&(pushflag == 0 ))
+        {
+            cout<<"I am IMU1"<<endl;
+            IMUinfo.GPSweek= *(unsigned short int*) (&(procbuff[startindex+3+0]));
+            IMUinfo.GPStime= *(unsigned int*) (&(procbuff[startindex+3+2]));
+            IMUinfo.GyroX = *(double *) (&(procbuff[startindex+3+6]));
+            IMUinfo.GyroY = *(double *) (&(procbuff[startindex+3+14]));
+            IMUinfo.Gyroz = *(double *) (&(procbuff[startindex+3+22]));
+            IMUinfo.AccX = *(double *) (&(procbuff[startindex+3+30]));
+            IMUinfo.AccY = *(double *) (&(procbuff[startindex+3+38]));
+            IMUinfo.AccZ = *(double *) (&(procbuff[startindex+3+46]));
+            IMUinfo.Yaw = *(float *)(&(procbuff[startindex+63+6]));
+            IMUinfo.Pitch = *(float *)(&(procbuff[startindex+63+10]));
+            IMUinfo.Roll = *(float *)(&(procbuff[startindex+63+14]));
+            IMUinfo.latitude = *(unsigned int *)(&(procbuff[startindex+63+18]));
+            IMUinfo.longitude = *(unsigned int *)(&(procbuff[startindex+63+22]));
+            IMUinfo.altitude = *(unsigned int *)(&(procbuff[startindex+63+26]));
+            IMUinfo.Ve = *(float *)(&(procbuff[startindex+63+30]));
+            IMUinfo.Va = *(float *)(&(procbuff[startindex+63+34]));
+            IMUinfo.Vu = *(float *)(&(procbuff[startindex+63+38]));
+            cout<<"IMUinfo.GPSweek = "<<IMUinfo.GPSweek<<endl;
+            cout<<"IMUinfo.GPStime = "<<IMUinfo.GPStime<<endl;
+            cout<<" IMUinfo.GyroX = "<< IMUinfo.GyroX<<endl;
+            cout<<" IMUinfo.GyroY = "<<IMUinfo.GyroY<<endl;
+            cout<<"IMUinfo.GyroZ = "<< IMUinfo.Gyroz<<endl;
+            cout<<"IMUinfo.AccX = "<< IMUinfo.AccX<<endl;
+            cout<<"  IMUinfo.AccY  = "<<  IMUinfo.AccY <<endl;
+            cout<<" IMUinfo.AccZ = "<<  IMUinfo.AccZ<<endl;
+            cout<<" IMUinfo.Yaw = "<< IMUinfo.Yaw<<endl;
+            cout<<" IMUinfo.Pitch = "<<IMUinfo.Pitch<<endl;
+            cout<<"IMUinfo.Roll = "<< IMUinfo.Roll<<endl;
+            cout<<" IMUinfo.latitude= "<< IMUinfo.latitude<<endl;
+            cout<<" IMUinfo.longitude = "<<   IMUinfo.longitude<<endl;
+            cout<<"IMUinfo.altitude  = "<<IMUinfo.altitude <<endl;
+            cout<<" IMUinfo.Ve= "<<  IMUinfo.Ve<<endl;
+            cout<<" IMUinfo.Va= "<< IMUinfo.Va<<endl;
+            cout<<" IMUinfo.Vu= "<<  IMUinfo.Vu<<endl;
             startflag = 0;
             startindex = 0;
         }
+        
     }
     procbuff[2000]={'+'};
-    startflag = 0;
-    startindex = 0;
-    lenthoftembuff = 0;
+    // startflag = 0;
+    // startindex = 0;
+    // lenthoftembuff = 0;
     return;
 }
 
 void IMU_datatran(sensor_msgs::Imu &temimu, GTIMU const temimudata, ros::Time const Time){
+    cout<<"enter imu_datatran"<<endl;
     tf::Quaternion q = tf::createQuaternionFromRPY((temimudata.Roll*3.1415926/180),(temimudata.Pitch*3.1415926/180), (temimudata.Yaw*3.1415926/180));
     
+    cout<<"q.x()="<<q.x()<<endl;
     temimu.header.stamp = Time;
     temimu.header.frame_id = "base_IMU";
     temimu.orientation.x = q.x();
@@ -233,6 +260,7 @@ void IMU_datatran(sensor_msgs::Imu &temimu, GTIMU const temimudata, ros::Time co
 }
 
 void GPS_datatran(sensor_msgs::NavSatFix &tmpGps, GTIMU const temimudata, ros::Time const Time){
+    cout<<"enter GPS_datatran"<<endl;
     if (temimudata.longitude == 0 ||
             temimudata.latitude == 0 ||
             temimudata.altitude == 0 ||
@@ -255,6 +283,7 @@ void GPS_datatran(sensor_msgs::NavSatFix &tmpGps, GTIMU const temimudata, ros::T
 }
 
 void Vel_datatran(geometry_msgs::Vector3Stamped tmpVel, GTIMU const temimudata, ros::Time const Time){
+    cout<<"enter Vel_datatran"<<endl;
     tmpVel.header.stamp = Time;
 
     if (abs(temimudata.Ve) <= 0.000000001
